734dc8115707410cbb40301cee1ff1d44e22d5d7
SABURJON <safarovsaburjon2002@gmail.com>
2021-09-28 15:06:54 +0500

    repet !!

---
+++ src/googleee.kt
@@ -0,0 +1,14 @@
+fun main() {
+    while (true) {
+        print("input text ->")
+        val b = readLine()!!.toString()
+        print("input repet - number ->")
+        val a = readLine()!!.toInt()
+        var count = 0
+        repeat(a) {
+            count += 1
+
+            println("#$count -> $b -> ${b.length} simbols ")
+        }
+    }
+}
